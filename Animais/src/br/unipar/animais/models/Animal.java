d32ec94b3d33803b691b0704dfa4917125f273d8
Bruno Vian <bruno.vian@edu.unipar.br>
2022-08-22 21:33:20 -0300

    Criado classe animal

--- Animais/src/br/unipar/animais/models/Animal.java
+++ Animais/src/br/unipar/animais/models/Animal.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.unipar.animais.models;
 
-/**
- *
- * @author bruno
- */
+
 public class Animal {
+    private String nome;
+    private double comprimento;
+    private int numPatas;
+    private String cor;
+    private String ambiente;
+    private int velocidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public int getNumPatas() {
+        return numPatas;
+    }
+
+    public void setNumPatas(int numPatas) {
+        this.numPatas = numPatas;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getAmbiente() {
+        return ambiente;
+    }
+
+    public void setAmbiente(String ambiente) {
+        this.ambiente = ambiente;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+    
     
 }
